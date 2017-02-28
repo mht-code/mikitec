387ac0bfdf84827798fc71deffdc86301de4f127
mht-miki-iimura <m.iimura@mht-code.com>
2017-02-28 17:12:24 +0900

    <ブランチの作成を失敗したのであげ直しました> Lesson1,2の複合アプリを作成しました。
    Lesson1.ボタンを押してハローワールドの表示 / storyboardを使用
    画面にhello worldを表示するアプリを作る　
    Lesson2.画面の追加 / storyboardを使用
    初期表示画面と、次に表示する画面の２つの画面を作り、ボタンを押すと画面を遷移できるアプリを作る
    それぞれの画面にボタンがついていて、押すと画面の行き来ができるアプリを作る

---
+++ Lesson1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lesson1
+//
+//  Created by 飯村美紀 on 2017/02/26.
+//  Copyright © 2017年 miki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
