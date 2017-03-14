687833e5756cf753699201fd769ac07efb19d9b3
mht-miki-iimura <m.iimura@mht-code.com>
2017-03-14 22:37:28 +0900

    StackViewを使用して3つの画像を等間隔に並べました

---
+++ AutoLayout_Lesson1/AutoLayout_Lesson1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayout_Lesson1
+//
+//  Created by 飯村美紀 on 2017/03/14.
+//  Copyright © 2017年 miki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
